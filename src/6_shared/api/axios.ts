bf3752825ab7d91783faabf622366a5fab7fa139
Birkerochek <solvy2007@gmail.com>
2025-12-23 00:06:19 +0500

    api

--- src/6_shared/api/axios.ts
+++ src/6_shared/api/axios.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") ?? ""}/`;
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
-
