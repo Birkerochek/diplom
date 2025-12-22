7ff7808d3b725fd1f5b5f977e67a195f5ed075f0
Birkerochek <solvy2007@gmail.com>
2025-12-22 21:38:45 +0500

    fix

--- src/6_shared/lib/prisma.ts
+++ src/6_shared/lib/prisma.ts
@@ -1,21 +1,12 @@
 import { PrismaClient } from "../../../app/generated/prisma";
 
-const datasourceUrl = process.env.DATABASE_URL;
-if (!datasourceUrl) {
-  throw new Error("DATABASE_URL is not set");
-}
-
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
   global.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: { url: datasourceUrl },
-    },
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
