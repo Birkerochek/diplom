0404ac684da2e18ac70d5874cdd3144f9be14812
Birkerochek <solvy2007@gmail.com>
2025-12-22 22:01:19 +0500

    fix

--- app/generated/prisma/index.d.ts
+++ app/generated/prisma/index.d.ts
@@ -338,8 +338,8 @@ export namespace Prisma {
   export import Exact = $Public.Exact
 
   /**
-   * Prisma Client JS version: 6.17.0
-   * Query Engine version: c0aafc03b8ef6cdced8654b9a817999e02457d6a
+   * Prisma Client JS version: 6.19.1
+   * Query Engine version: c2990dca591cba766e3b7ef5d9e8a84796e47ab7
    */
   export type PrismaVersion = {
     client: string
@@ -352,6 +352,7 @@ export namespace Prisma {
    */
 
 
+  export import Bytes = runtime.Bytes
   export import JsonObject = runtime.JsonObject
   export import JsonArray = runtime.JsonArray
   export import JsonValue = runtime.JsonValue
