57afa07e52736505978d5c4bebc60dbe673f42ba
Birkerochek <solvy2007@gmail.com>
2025-12-23 20:26:34 +0500

    fix

--- src/3_widgets/Header/ui/Header.tsx
+++ src/3_widgets/Header/ui/Header.tsx
@@ -28,7 +28,7 @@ export const Header = () => {
   return (
     <Container>
       <div className={s.header}>
-        <Logo /> фикс 2
+        <Logo /> фикс 3
         <div className={s.header__center}>
           {role === ROLES.ORGANIZER ? (
             <>
