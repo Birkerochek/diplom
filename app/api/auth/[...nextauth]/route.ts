b87d0b19cefe7123755f329f64fa9ed2544f7221
Birkerochek <solvy2007@gmail.com>
2025-12-23 20:12:10 +0500

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,6 @@
-export const runtime = 'nodejs';
+import NextAuth from "next-auth";
+import { nextAuthOptions } from "@shared/config/nextAuth";
 
-import NextAuth from 'next-auth';
-import { nextAuthOptions } from '@shared/config/nextAuth';
-import { headers, cookies } from 'next/headers';
+const handler = NextAuth(nextAuthOptions);
 
-const nextAuthHandler = NextAuth(nextAuthOptions);
-
-async function handlerWithLogs(req: Request) {
-  const h = await headers();
-  const c = await cookies();
-
-  console.log('--- AUTH REQUEST ---');
-  console.log('x-forwarded-proto:', h.get('x-forwarded-proto'));
-  console.log('host:', h.get('host'));
-  console.log('cookie header:', h.get('cookie'));
-  console.log('parsed cookies:', c.getAll());
-
-  return nextAuthHandler(req);
-}
-
-export { handlerWithLogs as GET, handlerWithLogs as POST };
+export { handler as GET, handler as POST };
