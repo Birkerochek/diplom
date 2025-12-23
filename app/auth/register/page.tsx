8b85db72ede4b2af6795dbdc022ff1d62ef61315
Birkerochek <solvy2007@gmail.com>
2025-12-23 22:46:47 +0500

    fix

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,9 +1,10 @@
 import { RegisterPage } from "@pages/RegisterPage";
+import { Suspense } from "react";
 
 export default function page() {
     return (
-        <div>
+        <Suspense fallback={<div>Loading...</div>}>
             <RegisterPage/>
-        </div>
+        </Suspense>
     );
 }
